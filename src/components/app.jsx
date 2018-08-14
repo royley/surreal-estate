cf01c79602d716ec3b63672eccaa707a2c170001
Stephen Royle <stephenjroyle@gmail.com>
2018-08-14 21:25:02 +0100

    surreal estate step 1 and some of step 2

---
+++ src/components/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../app.css';
+import NavBar from './navbar';
+
+const App = () => <NavBar />;
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../navbar.css';
+
+const NavBar = () =>
+  <div className="NavBar">
+    <div className="fas fa-home">Surreal Estate</div>
+    <ul className="nav">
+      <li className="item"></li>
+      <li className="item"></li>
+    </ul>
+  </div>
+
+export default NavBar;

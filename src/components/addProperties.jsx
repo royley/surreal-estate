16563cef0dc2fe325f85c8a64a569ac9ffc3e0c7
Stephen Royle <stephenjroyle@gmail.com>
2018-08-23 20:55:16 +0100

    surreal estate step 4 complete with inputs arranged

--- src/components/addProperties.jsx
+++ src/components/addProperties.jsx
@@ -39,34 +39,50 @@ class AddProperties extends React.Component {
               </div>
               <div>
                 <form onSubmit={this.handleAddProperty}>
-                  Property Title
-                  <input name="title" value={this.state.fields.title} type="string" onChange={this.handleFieldChange}/>
-                  Property Type
-                  <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
-                    <option value="Flat">Flat</option>
-                    <option value="Detached">Detached</option>
-                    <option value="Semi-Detached">Semi-Detached</option>
-                    <option value="Terraced">Terraced</option>
-                    <option value="End of Terrace">End of Terrace</option>
-                    <option value="Cottage">Cottage</option>
-                    <option value="Bungalow">Bungalow</option>
-                  </select>
+                  <div>
+                    Property Title
+                    <input name="title" value={this.state.fields.title} type="string" onChange={this.handleFieldChange}/>
+                  </div>
+                  <div>
+                    Property Type
+                      <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
+                      <option value="Flat">Flat</option>
+                      <option value="Detached">Detached</option>
+                      <option value="Semi-Detached">Semi-Detached</option>
+                      <option value="Terraced">Terraced</option>
+                      <option value="End of Terrace">End of Terrace</option>
+                      <option value="Cottage">Cottage</option>
+                      <option value="Bungalow">Bungalow</option>
+                    </select>
+                  </div>
+                  <div>
                   City
-                  <select name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
-                    <option value="Manchester">Manchester</option>
-                    <option value="Leeds">Leeds</option>
-                    <option value="Sheffield">Sheffield</option>
-                    <option value="Liverpool">Liverpool</option>
-                  </select>
-                  Bedrooms
-                  <input name="bedrooms" type="number" onChange={this.handleFieldChange}/>
-                  Bathrooms
-                  <input name="bathrooms" type="number"onChange={this.handleFieldChange}/>
-                  Price
-                  <input name="price" type="string"onChange={this.handleFieldChange}/>
-                  Email
-                  <input name="email" type="email"onChange={this.handleFieldChange}/>
-                  <button type="submit">Add</button>
+                    <select name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
+                      <option value="Manchester">Manchester</option>
+                      <option value="Leeds">Leeds</option>
+                      <option value="Sheffield">Sheffield</option>
+                      <option value="Liverpool">Liverpool</option>
+                    </select>
+                  </div>
+                  <div>
+                    Bedrooms
+                    <input name="bedrooms" type="number" onChange={this.handleFieldChange}/>
+                  </div>
+                  <div>
+                    Bathrooms
+                    <input name="bathrooms" type="number"onChange={this.handleFieldChange}/>
+                  </div>
+                  <div>
+                    Price
+                    <input name="price" type="string"onChange={this.handleFieldChange}/>
+                  </div>
+                  <div>
+                    Email
+                    <input name="email" type="email"onChange={this.handleFieldChange}/>
+                  </div>
+                  <div>
+                    <button type="submit">Add</button>
+                  </div>
                 </form>
               </div>
             </div>
